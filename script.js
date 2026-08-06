b225be0ac7a19cd09ef7e8b07457fc86b653e305
Istiak Alam <168295675+Istiaq-Alam@users.noreply.github.com>
2026-08-06 23:10:50 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,1 @@
-const textElement = document.getElementById("typewriter");
-const text = "Whispers of the Heart";
-let index = 0;
 
-function typeEffect() {
-    if (index < text.length) {
-        textElement.innerHTML += text.charAt(index);
-        index++;
-        setTimeout(typeEffect, 150); // Speed of typing in milliseconds
-    } else {
-        // Remove the blinking cursor after finishing
-        textElement.style.borderRight = "none";
-    }
-}
-
-// Start the animation when the window loads
-window.onload = typeEffect;ct;
